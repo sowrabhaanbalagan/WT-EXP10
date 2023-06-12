fe2f9f74b65e0ef63b138eaa1dbb32a01cf9b60b
sowrabhaanbalagan <132478626+sowrabhaanbalagan@users.noreply.github.com>
2023-06-12 14:04:39 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -2,28 +2,27 @@ import React, { Component } from 'react';
 class App extends React.Component {  
   constructor(props) {  
       super(props);  
-      this.state = {value: ''};  
-      this.handleChange = this.handleChange.bind(this);  
-      this.handleSubmit = this.handleSubmit.bind(this);  
+      this.updateSubmit = this.updateSubmit.bind(this);  
+      this.input = React.createRef();  
   }  
-  handleChange(event) {  
-      this.setState({value: event.target.value});  
-  }  
-  handleSubmit(event) {  
-      alert('You have submitted the input successfully: ' + this.state.value);  
+  updateSubmit(event) {  
+      alert('You have entered the UserName and CompanyName successfully.');  
       event.preventDefault();  
   }  
   render() {  
-      return (  
-          <form onSubmit={this.handleSubmit}>  
-            <h1>Controlled Form Example</h1>  
-            <label>  
-                Name:  
-                <input type="text" value={this.state.value} onChange={this.handleChange} />  
-            </label>  
-            <input type="submit" value="Submit" />  
-         </form>  
-      );  
+    return (  
+      <form onSubmit={this.updateSubmit}>  
+        <h1>Uncontrolled Form Example</h1>  
+        <label>Name:  
+            <input type="text" ref={this.input} />  
+        </label>  
+        <label>  
+            CompanyName:  
+            <input type="text" ref={this.input} />  
+        </label>  
+        <input type="submit" value="Submit" />  
+      </form>  
+    );  
   }  
 }  
 export default App;  
